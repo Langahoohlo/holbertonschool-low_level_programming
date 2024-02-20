9cd4730870ad93818d3d4dc074febfb05b16057d
langahoohlo <langahoohlo@gmail.com>
2024-02-20 09:32:03 +0000

    it iis like a puzzle

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - this file is the main function
+ *
+ * Return: is string
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
