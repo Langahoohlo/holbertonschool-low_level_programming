731664529b2fd67a274901384ff8f16939b39b45
langahoohlo <langahoohlo@gmail.com>
2024-02-21 13:16:08 +0000

    print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program to print letter
+ *
+ * Return: will return a string
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 10; n++)
+		printf("%d \n", n);
+	return (0);
+}
