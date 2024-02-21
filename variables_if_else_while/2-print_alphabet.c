d9157972f582d8f2bdc2758276ff9f257b7d50e5
langahoohlo <langahoohlo@gmail.com>
2024-02-21 11:05:38 +0000

    alphabets

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - program to print letter
+ *
+ * Return: will return a string
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a' ; a < 'z' ; a++)
+	putchar(a);
+	putchar('\n');
+	return (0);
+}
