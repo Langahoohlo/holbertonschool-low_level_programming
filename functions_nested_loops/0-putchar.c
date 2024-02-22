64742e9c53b2b3fcff6861d30598d77c9b5f2eb4
langahoohlo <langahoohlo@gmail.com>
2024-02-22 08:12:00 +0000

    putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - start of the program
+ *
+ * Return: will return 0
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
