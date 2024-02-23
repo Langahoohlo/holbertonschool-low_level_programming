0aa86070f94d91c3c4eb2d84b621d27ccedba84f
langahoohlo <langahoohlo@gmail.com>
2024-02-23 10:51:34 +0000

    alphabets

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Function to print alphabets
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
