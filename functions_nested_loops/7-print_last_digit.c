7e194222ced3109839d61acf692d2100bfc08488
langahoohlo <langahoohlo@gmail.com>
2024-02-23 14:26:07 +0000

    last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of n
+ * @n: recieved argument
+ * Return: lastdigit
+ */
+
+int print_last_digit(int n)
+{
+	int lastdigit = n & 10;
+
+	if (lastdigit < 0)
+	{
+		lastdigit = lastdigit * -1;
+	}
+	_putchar(lastdigit + '0');
+	return (lastdigit);
+}
