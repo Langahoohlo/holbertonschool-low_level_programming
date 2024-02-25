2f68e2e598dd04e1de775ba14ad3199bb1f7a22b
langahoohlo <langahoohlo@gmail.com>
2024-02-25 18:01:41 +0000

    to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_to_98 - print numbers number to 98
+* 
+* @num: integer argument
+*/
+
+void print_to_98(int num)
+{
+	if (num <= 98)
+	{
+		for (; num <= 98; num++)
+		{
+			printf("%d", num);
+			if (num != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+
+	else
+	{
+		for (; num >= 98; num--)
+		{
+			printf("%d", num);
+
+			if (num != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
