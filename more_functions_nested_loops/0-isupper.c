1b0398d785919939c4cbf9f132ae6ccd3b362cfd
langahoohlo <langahoohlo@gmail.com>
2024-02-26 10:21:59 +0000

    isupper case

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - check for lower case
+ * @c : is an argument recievd to check if islower
+ * Return : code exit
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c >=90)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
