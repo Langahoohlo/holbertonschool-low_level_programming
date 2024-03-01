bdc85607ac70ae29511939e5d6f77621b3f7e53b
langahoohlo <langahoohlo@gmail.com>
2024-03-01 19:46:58 +0000

    fixes

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,11 +1,17 @@
 #include "main.h"
 
+/**
+ * leet - function for leet
+ * @s: argument to change
+ * Return: always s
+ */
+
 char *leet(char *s)
 {
 	char find[] = "aAeEoOtTlL";
 	char mv[] = "4433007711";
 	int i, x;
-	
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (x = 0; find[x] != '\0'; x++)
@@ -17,6 +23,5 @@ char *leet(char *s)
 			}
 		}
 	}
-
-    return (s);
+	return (s);
 }
