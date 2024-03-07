fa715ca78ab78a3e89eb9fcb30bb879ea1153e9a
langahoohlo <langahoohlo@gmail.com>
2024-03-07 12:32:36 +0000

    4-add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main- entry point
+ * @argc: commandline argument
+ * @argv: commandline argument
+ * Return: 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, result = 0;
+
+	if (argc > 2)
+	{
+		for (i = 1; i < argc; i++)
+			if (isdigit(*argv[i]))
+			{
+				result += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", result);
+	return (0);
+}
