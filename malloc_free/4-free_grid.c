93f35cbabb2f2a3226f1d7d8b9110af71a55d6a4
langahoohlo <langahoohlo@gmail.com>
2024-03-12 17:23:39 +0000

    fixes

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -5,16 +5,18 @@
  * free_grid - function
  * @grid: argument
  * @height: argument
- * Return: grid
  */
 
 void free_grid(int **grid, int height)
 {
 	int i;
 
+	if (grid == NULL ||  height <= 0)
+		return;
+
 	for (i = 0; i < height; i++)
 	{
-		free(grid[i]);
+		free(grid[(height - 1) - i]);
 	}
 
 	free(grid);
