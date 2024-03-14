8609e310532209e4a0f358b0f83c5e5d2962e913
langahoohlo <langahoohlo@gmail.com>
2024-03-14 14:48:30 +0000

    dog_t

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -29,4 +29,9 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+typdef struct dog dog_t;
+dog_t *new_dog(struct dog *d, char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
 #endif
