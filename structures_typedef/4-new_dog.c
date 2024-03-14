ac53df839d2dc28ebe63c1eaff78037d7bd02590
langahoohlo <langahoohlo@gmail.com>
2024-03-14 20:20:12 +0000

    to be completed

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,15 +1,58 @@
 #include "dog.h"
 #include <stdlib.h>
 
-char *_strcpy(char *dest, char *src, int n)
+char _strcpy(char *dest, char *src)
 {
-	char *tmp = dest;
+	int i;
 
-	for (; n != 0 && *src != '\0'; n--, src++)
+	for (i = 0; src[i]; i++)
 	{
-		*tmp = *src;
-		tmp++;
+		dest[i] = src[i];
 	}
 
+	dest[i] = '\0';
+}
 
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	int name_length = 0;
+	int owner_length = 0;
+
+	for (name_length = 0; name[name_length]; name_length++)
+		;
+
+	for (owner_length = 0; owner[owner_length]; owner_length++)
+		;
+
+	new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+
+	new_dog->name = malloc(name_length + 1);
+
+	if (!new_dog->name)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+
+	new_dog->owner = malloc(owner_length + 1);
+
+	if (!new_dog->owner)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+
+	_strcpy(new_dog->name, name);
+	_strcpy(new_dog->owner, owner);
+
+	new_dog->age = age;
+
+	return (new_dog);
 }
