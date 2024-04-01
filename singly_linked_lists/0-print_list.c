1430083e4b26a724aec647b006ea1da1cc4b4586
langahoohlo <langahoohlo@gmail.com>
2024-04-01 11:12:34 +0000

    intro to lists in c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - function to print argument
+ * @h: argument to print
+ * return: h length
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int i = 0;
+
+	while (h != NULL)
+	{
+		printf("[%u] ", h->len);
+
+		if (h->str != NULL)
+			printf("%s\n", h->str);
+
+		else if (h->str == NULL)
+		{
+			printf("(nil)\n");
+		}
+
+		h = h->next;
+
+		i++;
+	}
+
+	return (i);
+}
