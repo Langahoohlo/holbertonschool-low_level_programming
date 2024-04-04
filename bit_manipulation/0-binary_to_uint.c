9a8b7862fe847b759b70e9db960254fe0220ef95
langahoohlo <langahoohlo@gmail.com>
2024-04-04 10:16:29 +0000

    task 0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+/**
+ * binary_to_uint - function to convert to binary
+ * @b: number to convert
+ * Return: i
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i = 0;
+
+	while (*b != '\0')
+	{
+		i <<= 1;
+
+		if (*b == '1')
+		{
+			i |= 1;
+		}
+		else if (*b != '0')
+		{
+			return (0);
+		}
+		b++;
+	}
+	return (i);
+}
